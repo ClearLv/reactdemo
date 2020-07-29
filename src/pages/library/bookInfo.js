fdb49c7afee422c6a1d0acee02cc8f7aa7d07d33
ClearLv <zr951561609@163.com>
2020-07-29 11:50:17 +0800

    上传图片,编辑信息

--- src/pages/library/bookInfo.js
+++ src/pages/library/bookInfo.js
@@ -2,39 +2,33 @@ import React,{useState , useEffect , useReducer ,useContext } from "react";
 import './library.css'
 import axios from '../../http/myAxios'
 import {Context} from "../../Home";
-import { Input , Button , Modal , Form , Select , Upload } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import { Input , Button , Modal , Form , Select , Upload , message } from 'antd';
+import { UploadOutlined , LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 
 const { Option } = Select;
-const fileList = [
-    // {
-    //     uid: '-1',
-    //     name: 'xxx.png',
-    //     status: 'done',
-    //     url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    //     thumbUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    // },
-    // {
-    //     uid: '-2',
-    //     name: 'yyy.png',
-    //     status: 'error',
-    // },
-];
+
 export const BookInfo = (props) => {
     const [bookInfo , setBookInfo] = useState({});
     const [modalShow , setModalShow] = useState(false);
     const [loadStatus , setLoadStatus] = useState(false);
     const [cateList , setCateList] = useState([]);
+    const [imageStatus ,setImageStatus] = useState({imageUrl:'',loading: false});
+    const [imageLink , setImageLink] = useState('');
     const [form] = Form.useForm();
 
     useEffect(() => {
         // console.log(props.match.params.bookId);
         getBookInfo(props.match.params.bookId);
     },[props])
-
+    const uploadButton = (
+        <div>
+            {imageStatus.loading ? <LoadingOutlined /> : <PlusOutlined />}
+            <div className="ant-upload-text">Upload</div>
+        </div>
+    );
     return(
         <div className='bookinfo-container'>
-            <Button onClick={(e) => {goToEdit(e)}} style={{marginLeft:50}} type="primary">添加</Button>
+            <Button onClick={(e) => {goToEdit(e)}} style={{marginLeft:50}} type="primary">编辑</Button>
             <img width={500} height={600} src={bookInfo.cover}/>
             <div className='bookinfo-context'>
                 <div className='bookinfo-label'>作者：{bookInfo.author}</div>
@@ -43,11 +37,11 @@ export const BookInfo = (props) => {
                 <div className='bookinfo-label'>出版社：{bookInfo.press}</div>
             </div>
             <Modal
-                title="添加"
+                title="编辑详情"
                 visible={modalShow}
                 onOk={() => addBook()}
                 confirmLoading={loadStatus}
-                onCancel={() => handleCancel}
+                onCancel={() => handleCancel()}
             >
                 <Form form={form} initialValues={bookInfo}>
                     <Form.Item name="title" label="书名">
@@ -63,16 +57,27 @@ export const BookInfo = (props) => {
                     <Form.Item name="press" label="出版社">
                         <Input placeholder="出版社" name='press'/>
                     </Form.Item>
-                    <Form.Item name="cover" label="封面">
+                    <Form.Item label="封面">
+                        {/*<Upload*/}
+                        {/*    action={ 'http://localhost:8080/uploadBookCover/'}*/}
+                        {/*    fileList={fileList}*/}
+                        {/*    listType={'picture'}*/}
+                        {/*    onSuccess={(e) => uploadSuccess(e)}*/}
+                        {/*    onRemove={(e) => deletePic(e)}>*/}
+                        {/*    <Button>*/}
+                        {/*        <UploadOutlined /> Upload*/}
+                        {/*    </Button>*/}
+                        {/*</Upload>*/}
                         <Upload
-                            action={ 'http://localhost:8080/uploadBookCover/'}
-                            fileList={fileList}
-                            listType={'picture'}
-                            onSuccess={(e) => uploadSuccess(e)}
-                            onRemove={(e) => deletePic(e)}>
-                            <Button>
-                                <UploadOutlined /> Upload
-                            </Button>
+                            name="avatar"
+                            listType="picture-card"
+                            className="avatar-uploader"
+                            showUploadList={false}
+                            beforeUpload={beforeUpload}
+                            action="http://localhost:8080/uploadBookCover/"
+                            onChange={(info) => handleChange(info)}
+                        >
+                            {imageStatus.imageUrl ? <img src={imageStatus.imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
                         </Upload>
                     </Form.Item>
                     <Form.Item name="abs" label="简介">
@@ -95,21 +100,18 @@ export const BookInfo = (props) => {
 
     function getBookInfo(bookId){
         axios.get('/api/getBookById',{params:{bookId:Number(bookId)}}).then((data) => {
-            // console.log(data);
             setBookInfo(data.data);
         });
     }
 
     function addBook() {
-
         let params = form.getFieldsValue();
-        console.log(params);
-        // axios.post('api/postBookInfo',params).then((data) => {
-        //     console.log(data);
-        // })
-
-        setModalShow(false);
-        setLoadStatus(false);
+        params['cover'] = imageLink;
+        params['id'] = props.match.params.bookId;
+        axios.post('/api/editBookInfo',params).then((data) => {
+            setModalShow(false);
+            setLoadStatus(false);
+        })
     }
 
     function handleCancel() {
@@ -122,28 +124,34 @@ export const BookInfo = (props) => {
             setModalShow(true);
         });
     }
-
-    function fileChange(e) {
-        console.log(fileList);
-        console.log(e);
+    function beforeUpload(file) {
+        const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+        if (!isJpgOrPng) {
+            message.error('You can only upload JPG/PNG file!');
+        }
+        const isLt2M = file.size / 1024 / 1024 < 2;
+        if (!isLt2M) {
+            message.error('Image must smaller than 2MB!');
+        }
+        return isJpgOrPng && isLt2M;
     }
+    function getBase64(img, callback) {
+        const reader = new FileReader();
+        reader.addEventListener('load', () => callback(reader.result));
+        reader.readAsDataURL(img);
 
-    function uploadSuccess(e) {
-        console.log(e);
-        console.log(fileList);
-        fileList.push( {
-
-            uid: (new Date()).getMilliseconds(),
-            name: (new Date()).getMilliseconds() + '.png',
-            status: 'done',
-            url: e,
-            thumbUrl: e,
-
-        })
-        console.log(fileList);
     }
-
-    function deletePic(e) {
-        console.log(e);
+    function handleChange(info) {
+        if (info.file.status === 'uploading') {
+            setImageStatus({loading: true});
+            return;
+        }
+        if (info.file.status === 'done') {
+            // Get this url from response in real world.
+            setImageLink(info.file.response)  ;
+            getBase64(info.file.originFileObj, imageUrl =>
+                setImageStatus({imageUrl:imageUrl,loading: false})
+            );
+        }
     }
 }
